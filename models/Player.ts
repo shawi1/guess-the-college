bc6f587a78eb72a5025d915ed3abdf884f53fb4a
Sean Hawi <seanhawi2004@gmail.com>
2025-04-13 19:27:14 -0700

    populated list of players and college information

--- models/Player.ts
+++ models/Player.ts
@@ -3,10 +3,12 @@ import mongoose, { Schema, model, models } from 'mongoose';
 const PlayerSchema = new Schema({
   name: String,
   college: String,
-  draftYear: Number,
-  draftPick: String,
-  team: String,
   position: String,
+  conference: String,
+  mascot: String,
+  colors: [String],
+  region: String,
+  collegeInitial: String,
 });
 
 const Player = models.Player || model("Player", PlayerSchema);
